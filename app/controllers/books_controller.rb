1382c5ea5fa6f9cae457de0f760d8e3e0bf0ef0c
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2023-12-15 13:15:50 +0000

    [Finish]　 User infos作成まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
   end
   
   def index
+    @books = Book.all
   end
   
   def show
@@ -13,12 +14,22 @@ class BooksController < ApplicationController
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
   
   def update
   end
   
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
+  root to: 'homes#top', as: 'top'
   get  'homes/about', to: 'homes#about', as: 'about'
   devise_for :users
   resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
