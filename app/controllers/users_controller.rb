0b32ffca92c76cdae2e9b6a8f249448fddc90c44
EC2 Default User <149689018+furumasa127@users.noreply.github.com>
2023-12-19 15:50:18 +0000

    [Finish]CRUDの実装まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def book_params
